f1c880f80d0ef3dff5a4e78379a0bd90fd82a37d
hamzaoda <hamzaodawk@gmail.com>
2022-03-16 14:36:02 +0200

    Edit the BMI_Claculator file

--- Day 2/BMI_Calculator.py
+++ Day 2/BMI_Calculator.py
@@ -1,6 +1,6 @@
-height= input("Please Enter your height : ")
-weight= input("Please Enter your weight : ")
+height= float(input("Please Enter your height : "))
+weight= int(input("Please Enter your weight : "))
 
-BMI=weight/height**2
+BMI=weight/height ** 2
 
 print("your BMI is : " + str(BMI))
