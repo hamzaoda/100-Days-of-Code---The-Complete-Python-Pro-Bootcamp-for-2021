0ecfe3e0d1c451a0bb4706dc7941cfc8a38ac173
hamzaoda <hamzaodawk@gmail.com>
2022-03-16 15:01:28 +0200

    add the Life_In_Weeks file

---
+++ Day 2/Life_In_Weeks.py
@@ -0,0 +1,9 @@
+age = int(input("Please Enter your age : "))
+remaining_age= 90-age
+days = remaining_age*365
+weeks = remaining_age*52
+months = remaining_age*12
+
+print(f"you have a :{days} days and : {weeks} weeks and : {months} months")
+
+
