e99ae191809accac2551f77159f3ea5d0c38e4ad
hamzaoda <hamzaodawk@gmail.com>
2022-03-16 14:26:34 +0200

    add the BMI claculator file

---
+++ Day 2/BMI_Calculator.py
@@ -0,0 +1,6 @@
+height= input("Please Enter your height : ")
+weight= input("Please Enter your weight : ")
+
+BMI=weight/height**2
+
+print("your BMI is : " + str(BMI))
---
+++ Day 2/day-2-1exercise.py
@@ -0,0 +1,5 @@
+the_number= input("Please Enter a Number: ")
+first_number=int(the_number[0])
+second_number=int(the_number[1])
+
+print(first_number + second_number)
