473ff76d95e9587c45b3a46429c52c9633f35358
hamzaoda <hamzaodawk@gmail.com>
2022-03-19 13:59:34 +0200

    add fizz buzz exercise

---
+++ Day 5/Fizz_Buzz.py
@@ -0,0 +1,9 @@
+for number in range(1 , 100):
+    if number % 5 ==0 and number % 3==0:
+        print("FizzBuzz")
+    elif number % 5 == 0 :
+        print("Buzz")
+    elif number % 3 == 0 :
+        print("Fizz")
+    else:
+        print(number)
