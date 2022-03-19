eca09fe82a0660300f8c334acf46d652a1b4266c
hamzaoda <hamzaodawk@gmail.com>
2022-03-19 13:52:27 +0200

    add the adding even numbers exercise

---
+++ Day 5/Adding_Even_Numbers.py
@@ -0,0 +1,4 @@
+sum_of_even_numbers= 1
+for even_number in range(2, 101 ,2):
+    sum_of_even_numbers +=even_number 
+print(f"the sum of the even numbers between 0 and 100 is {sum_of_even_numbers}")
