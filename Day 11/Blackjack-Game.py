a73104d7661530195aeacaeccd76d1e2389c017f
hamzaoda <hamzaodawk@gmail.com>
2022-03-26 15:45:45 +0300

    add the final project of blackkjack with enhanve

--- Day 11/Blackjack-Game.py
+++ Day 11/Blackjack-Game.py
@@ -19,7 +19,9 @@ def checkAce(score, some_list):
 def checkWinner(user_score, computer_score):
     if user_score > 21:
         print("you Have Lost")
-    if user_score > computer_score :
+    elif computer_score > 21:
+        print("you Have Win")
+    elif user_score > computer_score :
         print("you have Win")
     elif computer_score > user_score :
         print("you have Lost")
@@ -30,8 +32,9 @@ def printUserScore():
 def printComputerScore():
     print(f"the dealer Cards are : {computer_cards} and his score is {computer_score}")
 def low(score, some_list):
-    if score < 17:
+    while score < 17 :
         some_list.append(getCard())
+        score = sumOfCards(some_list)
         
     
 user_score = 0
@@ -47,48 +50,59 @@ while play:
     computer_cards = []
     print(Logo.logo)
     print("Welcome to My BlackJack Game!!!")
-    addCardToUser()
+    addCardToUser() 
     addCardToUser()
     addCardToComputer()
     addCardToComputer()
     user_score= sumOfCards(user_cards)
+    computer_score= sumOfCards(computer_cards)
     print(f"your cards are : {user_cards} and your score is {user_score}")
     print(f"the dealer first card is :{computer_cards[0]}")
-    another=input("Please Enter a 'y' to get another card and 'n' to pass : ")
-    if another == "y":
-        addCardToUser()
-        checkAce(user_score, user_cards)
-        checkAce(computer_score, computer_cards)
-        user_score= sumOfCards(user_cards)
-        computer_score= sumOfCards(computer_cards)
-        printUserScore()
-        printComputerScore()
-        low(user_score, user_cards)
-        low(computer_score, computer_cards)
-        user_score= sumOfCards(user_cards)
-        computer_score= sumOfCards(computer_cards)
-        printUserScore()
-        printComputerScore()
-        checkWinner(user_score, computer_score)
-    else:
-        checkAce(user_score, user_cards)
-        checkAce(computer_score, computer_cards)
-        user_score= sumOfCards(user_cards)
-        computer_score= sumOfCards(computer_cards)
-        printUserScore()
-        printComputerScore()
-        low(user_score, user_cards)
-        low(computer_score, computer_cards)
-        user_score= sumOfCards(user_cards)
-        computer_score= sumOfCards(computer_cards)
-        printUserScore()
-        printComputerScore()
-        checkWinner(user_score, computer_score)
-    answer = input("do you want to play 'y' for play and 'n' to exit : ")  
-    if answer == "n":
-        play = False    
-    else:
-        play = True
     
+    if (user_score == 21 or computer_score == 21) and (len(user_cards) == 2 or len(computer_cards) == 2):
+        print("------------------------------")
+        low(user_score, user_cards)
+        low(computer_score, computer_cards)
+        printUserScore()
+        printComputerScore()
+        checkWinner(user_score, computer_score)
+        print("------------------------------")
+    else:
+        another=input("Please Enter a 'y' to get another card and 'n' to pass : ")
+        if another == "y":
+            addCardToUser()
+            checkAce(user_score, user_cards)
+            checkAce(computer_score, computer_cards)
+            user_score= sumOfCards(user_cards)
+            computer_score= sumOfCards(computer_cards)
+            low(user_score, user_cards)
+            low(computer_score, computer_cards)
+            checkAce(user_score, user_cards)
+            checkAce(computer_score, computer_cards)
+            user_score= sumOfCards(user_cards)
+            computer_score= sumOfCards(computer_cards)
+            printUserScore()
+            printComputerScore()
+            checkWinner(user_score, computer_score)
+        else:
+            checkAce(user_score, user_cards)
+            checkAce(computer_score, computer_cards)
+            user_score= sumOfCards(user_cards)
+            computer_score= sumOfCards(computer_cards)
+            low(user_score, user_cards)
+            low(computer_score, computer_cards)
+            checkAce(user_score, user_cards)
+            checkAce(computer_score, computer_cards)
+            user_score= sumOfCards(user_cards)
+            computer_score= sumOfCards(computer_cards)
+            printUserScore()
+            printComputerScore()
+            checkWinner(user_score, computer_score)
+        answer = input("do you want to play again 'y' for play and 'n' to exit : ")  
+        if answer == "n":
+            play = False    
+        else:
+            play = True
+        
     
     
