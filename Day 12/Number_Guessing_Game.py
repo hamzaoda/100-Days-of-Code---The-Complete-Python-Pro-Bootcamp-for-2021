6ce3b39cf237dc9e036963df0f2567d5fc626079
hamzaoda <hamzaodawk@gmail.com>
2022-03-28 14:01:39 +0300

    add the Day 12 Project

--- Day 12/Number_Guessing_Game.py
+++ Day 12/Number_Guessing_Game.py
@@ -2,15 +2,20 @@ import random
 
 ATTEMPTS = 0
 
+def difficulty():
+    dif=input("Choose a difficulty. Type 'easy' or 'hard' : ").lower()
+    while dif != 'easy' and dif != 'hard':
+        dif = input("Please Choose a difficulty. Type 'easy' or 'hard' : ").lower()
+    if dif == "easy":
+        ATTEMPTS = 10
+    else:
+        ATTEMPTS = 5
+    return ATTEMPTS
+
+
 print("Welcome To The Guessing Game!!!")
 print("I'm Thinking of a Number between 1 and 100!!!")
-dif=input("Choose a difficulty. Type 'easy' or 'hard' : ").lower()
-while dif != 'easy' and dif != 'hard':
-    dif = input("Please Choose a difficulty. Type 'easy' or 'hard' : ").lower()
-if dif == "easy":
-    ATTEMPTS = 10
-else:
-    ATTEMPTS = 5
+ATTEMPTS = difficulty()
 number = random.randint(1, 10)
 print(f"You Have {ATTEMPTS} Attempts Remaining To Guess The Nummber")
 guessed_nummber = 0
